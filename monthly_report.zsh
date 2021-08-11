ca615821b183703b6e6330910e1691e5aa65009a
ccchen <33764630+conniecc1@users.noreply.github.com>
2021-08-11 03:51:36 -0400

    Update monthly_report.zsh

--- monthly_report.zsh
+++ monthly_report.zsh
@@ -1,5 +1,5 @@
 sudo -u analytics-privatedata kerberos-run-command analytics-privatedata hdfs dfs -ls
 
-sudo -u analytics-privatedata /home/cchen/venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html --execute /home/jiawang/superset-reporting/update_active_editors.ipynb
+sudo -u analytics-privatedata /home/cchen/venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html --execute /home/cchen/superset-reporting/update_active_editors.ipynb
 
-sudo -u analytics-privatedata /home/cchen/venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html --execute /home/jiawang/superset-reporting/update_interactions.pynb
+sudo -u analytics-privatedata /home/cchen/venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to html --execute /home/cchen/superset-reporting/update_interactions.pynb
